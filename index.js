0f4eadbae32c7dfefedd9bd823c76c67e34956b1
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2023-10-17 17:48:19 -0400

    Wrote prompt questions and passed the data to a function to write the file

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the project title?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Give a short description of the project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Give any necessary instructions to install your application',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Give a short description of how to use your application',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: 'Select a license',
+        choices: ['None', 'MIT', 'Other'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Inform the user how to contribute to the project',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions for testing the application',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'Enter your github username',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'Enter your email',
+        name: 'email'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log(fileName, data);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions).then((response) => { writeToFile('README.md', response)});
+}
 
-// Function call to initialize app
 init();
